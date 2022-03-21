964dc9758d513d28ff7b8e219b5eae4842b4ef80
Andreas Ανδρέου <andreas.andreou@sky.uk>
2022-03-21 11:03:25 +0200

    Use groovy properties for getters
    Trying it out to see how it looks and feels.

--- java-cli/app/src/test/groovy/com/pittacode/apihelper/json/ArrayObjectsFlattenerTest.groovy
+++ java-cli/app/src/test/groovy/com/pittacode/apihelper/json/ArrayObjectsFlattenerTest.groovy
@@ -7,7 +7,7 @@ class ArrayObjectsFlattenerTest extends Specification {
 
     var underTest = new ArrayObjectsFlattener(new ObjectFlattener())
 
-    def "flatten objects into array"() {
+    def "flatten objects in an array"() {
         given:
         var jsonString = """
 [
@@ -35,20 +35,47 @@ class ArrayObjectsFlattenerTest extends Specification {
     }
 ]
 """
-        var jsonArray = JsonParser.parseString(jsonString).getAsJsonArray()
-        assert jsonArray.get(0).getAsJsonObject().keySet().size() == 2
-        assert jsonArray.get(1).getAsJsonObject().keySet().size() == 2
+        var jsonArray = JsonParser.parseString(jsonString).asJsonArray
+        assert jsonArray.get(0).asJsonObject.keySet().size() == 2
+        assert jsonArray.get(1).asJsonObject.keySet().size() == 2
 
         when:
         var result = underTest.flattenObjectsIn(jsonArray)
         println(result.toString())
 
         then:
-        var firstObjectKeys = result.get(0).getAsJsonObject().keySet()
+        var firstObjectKeys = result.get(0).asJsonObject.keySet()
         firstObjectKeys.size() == 6
         firstObjectKeys.containsAll("k11.k21", "k11.k22", "k11.k23", "k12.k21", "k12.k22", "k12.k23")
-        var secondObjectKeys = result.get(1).getAsJsonObject().keySet()
+        var secondObjectKeys = result.get(1).asJsonObject.keySet()
         secondObjectKeys.size() == 4
         secondObjectKeys.containsAll("k11.k21", "k11.k22", "k12.k21", "k12.k25")
+    }
+
+    def "flatten deeeeep nested objects in an array"() {
+        given:
+        var jsonString = """
+[
+    {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"value":1}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}},
+    {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"value":2}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}},
+    {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"value":3}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+]
+"""
+        var jsonArray = JsonParser.parseString(jsonString).asJsonArray
+
+        when:
+        var result = underTest.flattenObjectsIn(jsonArray)
+        println(result.toString())
+
+        then:
+        var firstObjectEntries = result.get(0).asJsonObject.entrySet()
+        firstObjectEntries.size() == 1
+        firstObjectEntries.getAt(0).value.asInt == 1
+        var secondObjectEntries = result.get(1).asJsonObject.entrySet()
+        secondObjectEntries.size() == 1
+        secondObjectEntries.getAt(0).value.asInt == 2
+        var thirdObjectEntries = result.get(2).asJsonObject.entrySet()
+        thirdObjectEntries.size() == 1
+        thirdObjectEntries.getAt(0).value.asInt == 3
     }
 }
--- java-cli/app/src/test/groovy/com/pittacode/apihelper/json/ObjectFlattenerTest.groovy
+++ java-cli/app/src/test/groovy/com/pittacode/apihelper/json/ObjectFlattenerTest.groovy
@@ -17,7 +17,7 @@ class ObjectFlattenerTest extends Specification {
   }
 }
 """
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -43,7 +43,7 @@ class ObjectFlattenerTest extends Specification {
                   "l9": -9999
                 }}}}}}}}}
 """
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -71,7 +71,7 @@ class ObjectFlattenerTest extends Specification {
   "5": ["more text", "some more text"]
 }
 """
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -85,7 +85,7 @@ class ObjectFlattenerTest extends Specification {
     def "flatten empty object"() {
         given:
         var jsonString = """{}"""
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -98,7 +98,7 @@ class ObjectFlattenerTest extends Specification {
     def "flatten nested empty object"() {
         given:
         var jsonString = """{"1": "v1", "2":{}}"""
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -112,7 +112,7 @@ class ObjectFlattenerTest extends Specification {
     def "flatten deply nested empty object"() {
         given:
         var jsonString = """{"1": "v1", "2":{ "3" : { "4": {}}}, "topLevel":0}"""
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
@@ -126,7 +126,7 @@ class ObjectFlattenerTest extends Specification {
     def "flatten deeeeeeeeeeeply nested empty object"() {
         given:
         var jsonString = """{"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": { "l1": {"l2": {"l3": {"l4": {"l5": {"l6": {"l7": {"l8": {"l9": {"l10": {"l11": {"l12": {"l13": {"l14": {"l15": {"l16": {"l17": {"l18": {"l19": {"l20": {"l21": {"l22": {"l23": {"l24": {"l25": {"l26": {"l27": {"l28": {"l29": {"l30": {"l31": {"l32": {"l33": {"l34": {"l35": {"l36": {"l37": {"l38": {"l39": {"l40": {"value":"value"}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"""
-        var jsonObject = JsonParser.parseString(jsonString).getAsJsonObject()
+        var jsonObject = JsonParser.parseString(jsonString).asJsonObject
 
         when:
         var result = underTest.flatten(jsonObject)
